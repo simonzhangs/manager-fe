6c02ac8558c3a8343c2e5b39692c95c3e348cc83
simonzhangs <2863389578@qq.com>
2022-05-22 20:42:00 +0800

    feat:架构设计-环境配置

---
+++ src/config/index.js
@@ -0,0 +1,24 @@
+/*
+ * 环境配置封装
+ * @date
+ */
+const env = import.meta.env.MODE || "prod";
+const EnvConfig = {
+  dev: {
+    baseApi: "/",
+    mockApi: "https://www.fastmock.site/mock/e7aee5c4f59f03ff4b6b33d30d6b9a8b/api",
+  },
+  test: {
+    baseApi: "//test.baidu.com/api",
+    mockApi: "https://www.fastmock.site/mock/e7aee5c4f59f03ff4b6b33d30d6b9a8b/api",
+  },
+  prod: {
+    baseApi: "//baidu.com/api",
+    mockApi: "",
+  },
+};
+export default {
+  env,
+  mock: true,
+  ...EnvConfig[env]
+};
--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import router from "./router";
 import ElementPlus from "element-plus";
 import 'element-plus/dist/index.css';
 
+import config from './config'
+import axios from "axios";
+
+axios.get(config.mockApi+ '/login').then(res => {
+    console.log(res);
+})
+
+
 console.log("env =>", import.meta.env);
 const app = createApp(App);
 app.use(router).use(ElementPlus).mount("#app");
